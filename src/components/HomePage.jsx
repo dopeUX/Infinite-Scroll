580ee4c6bb4946fd47d0e761826bc15d80d94e62
UXNation <uxnation.inc@gmail.com>
2022-05-09 20:18:31 +0530

    final

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,13 +1,55 @@
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import ImageCard from "./ImageCard";
+import {Column, Table} from 'react-virtualized';
+import {List, AutoSizer, CellMeasurerCache, CellMeasurer} from 'react-virtualized';
+
 
 export default function HomePage(){
+  const listHeight = 900;
+  const rowHeight = 300;
+  const rowWidth = 1200;
+  const cache = new CellMeasurerCache({
+  defaultWidth: 500,
+  defaultHeight: 900
+});
     const [images, setImages] = useState([]);
     const [fetching, setFetching] = useState(false);
     const limit = 6;
-    let n=0;
+    let n = 0;
     const r = require.context(process.env.PUBLIC_URL+'/public/images/', false, /\.(png|jpe?g|svg)$/);
+   
+    const Row = ({ index,key, style ,parent}) => {
+    //  const img = r.keys()[index].replace('./','');
+      let i = 3*index;
+      let arr = [];
+      for(let j=0;j<3;j++){
+       try{ 
+       arr.push(r.keys()[i].replace('./',''));
+       }catch(err){
+         console.log(err.message);
+       }
+       i++;
+      }
+      return  <CellMeasurer
+            key={key}
+            cache={cache}
+            parent={parent}
+            columnIndex={0}
+            rowIndex={index}>
+            {
+              
+            }
+               <div key={key} style={style} className="images-section">
+                  {
+                    arr.map((x,i)=>{
+                      return <ImageCard key={i} image={x}/>
+                    })
+                  }
+                </div>
+      </CellMeasurer>
+    };
+  
     
     const handleScroll = (e) => {
         // console.log(e.target.documentElement.scrollTop);
@@ -25,19 +67,20 @@ export default function HomePage(){
       async function loadImages(r) {
         setFetching(true)
         let images = [];
-        console.log(n)
-         for(let i=n;i<n+limit;i++){
-            try{ 
-            const a = await r.keys()[i];
-            await images.push(a.replace('./',''));
-            }
-            catch(err){
-               console.log(err.message); 
-               window.removeEventListener('scroll', handleScroll);
-            }
+        // console.log(n)
+        //  for(let i=n;i<n+limit;i++){
+        //     try{ 
+        //     const a = await r.keys()[i];
+        //     await images.push(a.replace('./',''));
+        //     }
+        //     catch(err){
+        //        console.log(err.message); 
+        //        window.removeEventListener('scroll', handleScroll);
+        //     }
 
-         }
-         setFetching(false);
+        //  }
+        //  setFetching(false);
+
          return images;
     }
 
@@ -50,23 +93,19 @@ export default function HomePage(){
         // console.log(images)
         window.addEventListener('scroll', handleScroll);
     },[]);
-
+   const len = r.keys().length%3;
+   const l = len===0?r.keys().length/3:parseInt(r.keys().length/3)+1;
     return (
         <div className="home-page" >
            <Header/> 
-           <div  className="images-section">
-               {
-                 images.map((x,i)=>{
-                     
-                     return(
-                         <ImageCard key={i} 
-                         image={x}/>
-                     )
-                 })
-               }
-               {
-                 fetching?<h2>Loading more...</h2>:null 
-               }
+          <div className="list">
+            
+           <List
+             width={rowWidth}
+             height={listHeight}
+             rowHeight={rowHeight}
+             rowRenderer={Row}
+             rowCount={l} />
            </div>
         </div>
     )
